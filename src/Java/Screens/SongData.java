45c145f4c34a6d48a02ece20abd2ac201299d1af
wilman2019 <154097752+wilman2019@users.noreply.github.com>
2024-04-07 13:01:31 -0500

    Create SongData.java

---
+++ src/Java/Screens/SongData.java
@@ -0,0 +1,13 @@
+package Screens;
+
+public class SongData {
+    private static String[][] songData;
+
+    public static void setSongData(String[][] data) {
+        songData = data;
+    }
+
+    public static String[][] getSongData() {
+        return songData;
+    }
+}
