b08641a367cb8d7982b8f7e5b7529500fbbb5266
wilman2019 <154097752+wilman2019@users.noreply.github.com>
2024-04-07 13:01:59 -0500

    Create StoreSelected.java

---
+++ src/Java/Screens/StoreSelected.java
@@ -0,0 +1,15 @@
+package Screens;
+
+import java.util.ArrayList;
+
+public class StoreSelected {
+    static ArrayList<String[]> list = new ArrayList<>();
+
+    public static void set(ArrayList<String[]> newList) {
+        list = newList;
+    }
+    
+    public static ArrayList<String[]> getList() {
+    	return list;
+    }
+}
